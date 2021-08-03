438927dabd0a91db2b5d19f99ecb211f594e8f46
Juan Douglas <juandouglas2004@gmail.com>
2021-08-02 23:05:40 -0300

    Add web site

---
+++ Account/Company/js/account_register.js
@@ -0,0 +1,114 @@
+const xhr = new XMLHttpRequest();
+var host = window.location.protocol + '//' + window.location.host + '/';
+
+function nextStepClick() {
+    var form = document.getElementById('frmCreateAccount');
+    var json = {
+        "Name": form.Name.value,
+        "Email": form.Email.value,
+        "Password": form.Password.value,
+        "ConfirmPassword": form.ConfirmPassword.value,
+        "PhoneNumber": form.PhoneNumber.value,
+        "AcceptTerms": form.AcceptTerms.checked
+    }
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('PUT', form.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF8;');
+    xhr.origin = origin;
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status == 400) {
+            var errors = xhr.response.errors;
+            Object.entries(errors).forEach(entry => {
+                const [key, value] = entry;
+                enableError(key, value);
+                console.log(key, value);
+            });
+
+        } else if (status == 200) {
+            loginAccount(form.Email.value, form.ConfirmPassword.value);
+            show("confirmAccountModal");
+        }
+        loadingHide();
+    };
+    loadingShow();
+    xhr.send(JSON.stringify(json));
+}
+
+function isAuthenticated() {
+    var cookies = getCookie("Authentication");
+
+}
+
+function loadingShow() {
+    var loading = document.getElementById('loading');
+    loading.style.display = 'block';
+
+    var form = document.getElementById('frmCreateAccount');
+    form.style.display = 'none';
+
+    var buttons = document.getElementById('buttons');
+    buttons.style.display = 'none';
+}
+
+function loadingHide() {
+    var loading = document.getElementById('loading');
+    loading.style.display = 'none';
+
+    var form = document.getElementById('frmCreateAccount');
+    form.style.display = 'block';
+
+    var buttons = document.getElementById('buttons');
+    buttons.style.display = 'inline-flex';
+}
+
+function sendEmailConfirmation() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', host + 'api/Account/SendEmailConfirmation', true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF8;');
+    xhr.origin = origin;
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+
+    };
+    xhr.send();
+}
+
+function loginAccount(user, password) {
+    var xhr = new XMLHttpRequest();
+    var url = host + "Api/login/FirstStep?user=" + user;
+    xhr.open('GET', url, true);
+    xhr.orgin = origin;
+    xhr.withCredentials = true;
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        url = host + 'api/Login/SecondStep?set_cookie=true&fs_id=' + xhr.response.id + '&key=' + xhr.response.key + '&pwd=' + password;
+        xhr.open('GET', url, true);
+        xhr.onload = function () {
+            sendEmailConfirmation();
+        }
+        xhr.send();
+    };
+    xhr.send();
+}
+
+function confirmCode() {
+    var xhr = new XMLHttpRequest();
+    var code = document.getElementById('ConfirmationCode').value;
+    var url = host + 'api/Account/ConfirmEmail?code=' + code + '&email=' + document.getElementById('frmCreateAccount').Email.value;
+    xhr.open('POST', url, true);
+    xhr.orgin = origin;
+    xhr.withCredentials = true;
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status != 200) {
+            enableError('ConfirmationCode', 'Codigo invalido!');
+        } else {
+            window.location.href = 'RegisterCompany.html';
+        }
+    };
+    xhr.send();
+}
---
+++ Account/Company/js/company_register.js
@@ -0,0 +1,109 @@
+var host = window.location.protocol + '//' + window.location.host + '/';
+
+function zipCodeKeyUp() {
+    var field = document.getElementById('WithdrawalCep');
+    zipCodeMask(field);
+}
+
+function createCompanyClick() {
+    var form = document.getElementById('frmCompany');
+    var frmCategory = document.getElementById('CompanyCategory');
+    var url = host + 'api/Account/Company/SearchCompanyCategory?unique=true&page=0&search_key=' + frmCategory.value;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.withCredentials = true;
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status = 200) {
+            var response = xhr.response;
+            var json =
+            {
+                "name": form.Name.value,
+                "description": null,
+                "adressCompany": null,
+                "adressDelivery": null,
+                "adressWithdrawal": null,
+                "companyCategory": response.id,
+                "cpfOrCnpj": form.CpfOrCnpj.value,
+                "phoneNumber": form.PhoneNumber.value,
+                "billingMonthly": parseFloat(form.BillingMonthly.value).toFixed(2),
+                "employeesNumber": form.EmployeesNumber.value,
+                "ownDelivery": form.OwnerDelivery.checked,
+                "openingHours": null
+            }
+
+            xhr.origin = origin;
+            xhr.open('PUT', form.action, true);
+            xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8;');
+            xhr.responseType = 'json'
+            xhr.onload = function () {
+                var status = xhr.status;
+                if (status == 400) {
+                    var errors = xhr.response.errors;
+                    Object.entries(errors).forEach(entry => {
+                        const [key, value] = entry;
+                        enableError(key, value);
+                        console.log(key, value);
+                    });
+                }
+
+                if (status == 200) {
+                    var modal = document.getElementById('CompanyAdressModal');
+                    modal.style.display = 'block';
+                }
+            }
+            xhr.send(JSON.stringify(json));
+        }
+        if (status == 404) {
+            enableError('CompanyCategory', 'Selecione uma categoria valida!');
+        }
+    }
+    if (frmCategory.value.length > 1) {
+        xhr.send();
+    } else {
+        enableError('CompanyCategory', 'Selecione uma categoria!');
+    }
+}
+
+function searchCategories() {
+    var url = host + 'api/Account/Company/SearchCompanyCategory?page=0&search_key=';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.withCredentials = false;
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status = 200) {
+            var datalist = document.getElementById('CategoriesList');
+            Object.entries(xhr.response).forEach(entry => {
+                const [position, category, description] = entry;
+                datalist.innerHTML = datalist.innerHTML + '<option value="' + category.name + '">' + category.description + '</option>';
+            });
+        }
+    }
+    xhr.send();
+}
+
+
+function loadingShow() {
+    var loading = document.getElementById('loading');
+    loading.style.display = 'block';
+
+    var form = document.getElementById('frmCreateAccount');
+    form.style.display = 'none';
+
+    var buttons = document.getElementById('buttons');
+    buttons.style.display = 'none';
+}
+
+function loadingHide() {
+    var loading = document.getElementById('loading');
+    loading.style.display = 'none';
+
+    var form = document.getElementById('frmCreateAccount');
+    form.style.display = 'block';
+
+    var buttons = document.getElementById('buttons');
+    buttons.style.display = 'inline-flex';
+}
